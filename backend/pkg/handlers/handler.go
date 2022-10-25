08475054c49a5d56fc0c2e182e02dbc7ff099728
natanbc <natanbc@users.noreply.github.com>
2022-10-25 18:48:22 +0000

    feat: Verify authentication tokens

--- backend/pkg/handlers/handler.go
+++ backend/pkg/handlers/handler.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+    "net/url"
+    "os"
+
 	"github.com/courselab/pollex/pollex-backend/pkg/controllers"
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +23,7 @@ func NewHandler(p *Params) *handler {
 	}
 
 	h.routePing(p.Router)
-	h.routeUsers(p.Router)
+	h.authRoutes(p.Router)
 
 	return h
 }
@@ -29,7 +32,21 @@ func (h *handler) routePing(router *gin.Engine) {
 	router.GET("/ping", h.ping)
 }
 
-func (h *handler) routeUsers(router *gin.Engine) {
+func (h *handler) authRoutes(router *gin.Engine) {
+    //TODO: figure out a better place for this configuration
+    base := os.Getenv("AUTH_SERVICE_URL")
+    baseUrl, err := url.Parse(base)
+    if len(base) == 0 || err != nil {
+        panic("Invalid or missing auth service base url")
+    }
+
+    group := router.Group("/")
+    group.Use(checkAuth(baseUrl))
+
+    h.routeUsers(group)
+}
+
+func (h *handler) routeUsers(router *gin.RouterGroup) {
 	router.GET("/users", h.getUsers)
 	router.GET("/users/:id", h.getUser)
 	router.POST("/users", h.createUser)
